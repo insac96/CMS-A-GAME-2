eab8b3df4d730b474bc5aa614f48a75c5bd42cad
Your Name <you@example.com>
2024-03-21 10:06:54 +0700

    Update

--- src/server/api/game/start.get.ts
+++ src/server/api/game/start.get.ts
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
     const config = await DB.Config.findOne().select('game enable.play') as IDBConfig
     if(!config) throw 'Không tìm thấy cấu hình trò chơi'
     if(!config.game.api.start) throw 'Trò chơi đang bảo trì'
-    if(auth.type < 1 && !config.enable.play) throw 'Trò chơi đang bảo trì'
+    // if(auth.type < 1 && !config.enable.play) throw 'Trò chơi đang bảo trì'
 
     const url = await gameStart(event, auth.username)
 
