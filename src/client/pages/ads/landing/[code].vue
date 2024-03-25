decc1c47b9e70b5b80f982175b0672a6ce160c6a
Your Name <you@example.com>
2024-03-25 15:56:43 +0700

    Update

--- src/client/pages/ads/landing/[code].vue
+++ src/client/pages/ads/landing/[code].vue
@@ -38,30 +38,6 @@ definePageMeta({
   layout: 'ads',
 })
 
-useHead({
-  script: [
-    { children: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '${landing.value ? landing.value.facebook_ads : null}');
-      fbq('track', 'PageView');
-    `},
-    { children: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer', '${landing.value ? landing.value.google_ads : null}');
-    `}
-  ],
-})
-
 const runtimeConfig = useRuntimeConfig()
 const route = useRoute()
 const authStore = useAuthStore()
@@ -102,6 +78,12 @@ const getLanding = async () => {
   try {
     const data = await useAPI('ads/landing/code', { code: route.params.code })
     landing.value = data
+
+    // Facebook Ads
+    if(landing.value.facebook_ads && !!process.client){
+      window.fbq('init', landing.value.facebook_ads);
+      window.fbq('track', 'PageView');
+    }
   }
   catch (e) {
     return false
